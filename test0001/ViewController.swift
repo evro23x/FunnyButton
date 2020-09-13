e6a2c76ddfd0118a7ed849abe4fab96f10cb29b7
Evgeny Gurov <egurov@ozon.travel>
2020-09-13 23:25:08 +0300

    Initial Commit

---
+++ test0001/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test0001
+//
+//  Created by Evgeniy Gurov on 13.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
